7d7476a4dba7a29e322f1534586e6e9706dda4d1
Azkar <https://github.com/nermien990/Azkar>
2021-01-03 18:56:25 +0200

    no commit

--- app/src/main/java/com/example/mynewkotlin/MainActivity.kt
+++ app/src/main/java/com/example/mynewkotlin/MainActivity.kt
@@ -4,21 +4,22 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.databinding.DataBindingUtil
+import com.example.mynewkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        morning_id.setOnClickListener{
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.morningId.setOnClickListener{
             var intent = Intent(this, MorningActivity:: class.java)
             startActivity(intent)
-
         }
 
-        evening_id.setOnClickListener {
+        binding.eveningId.setOnClickListener {
             var intent = Intent(this, EveningActivity:: class.java)
             startActivity(intent)
         }
